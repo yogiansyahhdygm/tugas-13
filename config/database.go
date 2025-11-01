89af374f30e33f3ab72b75dbe95719ef9623600c
yogiansyahhdygm <yogiansyah.hdygm@gmail.com>
2025-11-01 07:43:32 +0700

    upload tugas-15

--- config/database.go
+++ config/database.go
@@ -4,20 +4,37 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
 var DB *sql.DB
 
 func ConnectDatabase() {
-	var err error
+	// Load .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Gagal memuat file .env")
+	}
 
-	connStr := "host=localhost user=postgres password=admin dbname=bioskop_db sslmode=disable"
+	host := os.Getenv("DB_HOST")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+	port := os.Getenv("DB_PORT")
+	sslmode := os.Getenv("DB_SSLMODE")
+
+	connStr := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
+		host, user, password, dbname, port, sslmode,
+	)
+
 	DB, err = sql.Open("postgres", connStr)
 
 	if err != nil {
-		log.Fatal("Gagal koneksi ke database:", err)
+		log.Fatal("Gagal konek ke database:", err)
 	}
 
 	err = DB.Ping()
